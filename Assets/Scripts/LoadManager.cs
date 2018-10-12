b4fc8a4d523f159a29e62ebfd2ea3eb43c8da5ca
LEE@Desktop <wmfehdgml@gmail.com>
2018-10-13 00:39:19 +0900

    マネージャーシーンを作成

---
+++ Assets/Scripts/LoadManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadManager
+{
+    
+	[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    static void LoadManagerScene()
+    {
+        Debug.Log("Im in");
+        string SceneName = "ManagerScene";
+        if(!SceneManager.GetSceneByName(SceneName).IsValid())
+        {
+            SceneManager.LoadScene(SceneName, LoadSceneMode.Additive);
+        }
+
+    }
+}
