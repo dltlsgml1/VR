5d59232970d26cfb0f441817f2751b556e0acd7f
LEE@NotePC <wmfehdgml@gmail.com>
2018-10-30 11:23:46 +0900

    コントローラチェックOK

--- Assets/Scripts/ControllerManager.cs
+++ Assets/Scripts/ControllerManager.cs
@@ -1,16 +1,79 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class ControllerManager : MonoBehaviour {
+    public enum KeyList { Trigger,PadClick}
+    public static ControllerManager Instance = null;
+    public bool MainTrigger;
+    public bool PadTrigger;
+    
+    
+    private SteamVR_TrackedObject trackedObj;
+    private SteamVR_Controller.Device Controller
+    {
+        get { return SteamVR_Controller.Input((int)trackedObj.index); }
+    }
 
-	// Use this for initialization
-	void Start () {
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else if (Instance != null)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+
+    }
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        
 	}
+
+    public bool Get(int Input)
+    {
+        switch(Input)
+        {
+            case (int)KeyList.Trigger:
+                if(OVRInput.Get(OVRInput.Button.PrimaryHandTrigger))
+                {
+                    return true;
+                }
+                break;
+            case (int)KeyList.PadClick:
+                if (OVRInput.Get(OVRInput.Button.PrimaryTouchpad))
+                {
+                    return true;
+                }
+                break;
+        }
+        return false;
+    }
+    public bool GetDown(int Input)
+    {
+        switch (Input)
+        {
+            case (int)KeyList.Trigger:
+                if (OVRInput.GetDown(OVRInput.Button.PrimaryHandTrigger))
+                {
+                    return true;
+                }
+                break;
+            case (int)KeyList.PadClick:
+                if (OVRInput.GetDown(OVRInput.Button.PrimaryHandTrigger))
+                {
+                    return true;
+                }
+                break;
+        }
+        return false;
+    }
+
 }
---
+++ Assets/Scripts/ScriptManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ScriptManager : MonoBehaviour {
+    public Text text;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(ControllerManager.Instance.Get((int)ControllerManager.KeyList.Trigger))
+        {
+            text.text = "Trigger";
+        }
+
+        else if(ControllerManager.Instance.Get((int)ControllerManager.KeyList.PadClick))
+        {
+            text.text = "Pad";
+        }
+
+        else
+        {
+            text.text = "Nothing";
+        }
+        
+	}
+}
